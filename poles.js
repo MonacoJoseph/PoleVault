3917b114786b37730df511a73c902dac3cdab38b
MonacoJoseph <jcm132@pitt.edu>
2018-03-08 18:04:23 -0500

    Adding starter files

---
+++ poles.js
@@ -0,0 +1,20 @@
+function calculate() {
+    let flex = document.getElementById('flex').value;
+    let mass = document.getElementById('mass').value;
+    let poleLength = document.getElementById('poleL').value;
+    let height = document.getElementById('height').value;
+    let angle = document.getElementById('angle').value;
+
+    let angleinDegrees = (angle / 2.0) * Math.PI / 180.0;
+    let k = 51 * (1 - flex) + 2327;
+    let compress = 0.03279 * (poleLength - 4.265) + 0.3;
+    let tmp = Math.pow(compress, 2) * Math.pow(poleLength, 2);
+
+    let a = (k * tmp) / (2.0 * mass * 9.8);
+    let b = poleLength * (1 - compress) - (height / 4.0) - 0.2;
+    let c = height * Math.cos(angleinDegrees) / 6.0;
+    let result = document.getElementById('result');
+
+    result.innerHTML = (a + b + c).toFixed(2) + " meters";
+
+}
