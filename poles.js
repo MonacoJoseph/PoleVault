81ee8d32b0ca2852ce0a1c58136fda458acc4768
MonacoJoseph <cuzzz11@gmail.com>
2018-03-10 19:42:55 -0500

    Working on physics

--- poles.js
+++ poles.js
@@ -1,28 +1,60 @@
 let jumpHeight = 0;
+let man;
+let xPos = 0;
+let yPos = 0;
 
 function setup() {
     createCanvas(1024, 480);
+    man = new StickMan();
 }
 
 function draw() {
+    background(50, 89, 100);
     let xOrg = 320;
     let yOrg = 240;
     
     translate(xOrg, yOrg);
     strokeWeight(2);
     // Stick figure
-    line(0, 0, 0, 75);
-    line(0, 30, 35, 35);
-    line(0, 30, -35, 35);
-    line(0, 75, 25, 35+75);
-    line(0, 75, -25, 35+75);
-    ellipse(0, 0, 30, 30);
+    //line(xPos, yPos, xPos, yPos+75); // Body
+    //line(xPos, yPos+30, xPos+35, yPos+35); // Right Arm
+    //line(xPos, yPos+30, xPos-35, yPos+35); // Left Arm
+    //line(xPos, yPos+75, xPos+25, yPos+35+75); // Right Leg
+    //line(xPos, yPos+75, xPos-25, yPos+35+75); // Left Leg
+    //ellipse(xPos, yPos, 30, 30); // Head
+    for(let i=0; i < 100; i++) {
+        man.move();
+        man.display();
+    }
+    
+    xPos = xPos + 1;
+    yPos = yPos - 1;
+    if(xPos > 75) xPos = 0;
+    if(yPos < -200) yPos = 0;
+    
     
     // Pole
     translate(xOrg, yOrg-240);
+    strokeWeight(3);
     line(0, -100, 0, 100);
     line(50, -100, 50, 100);
     line(0, -90, 50, -90);
+}
+
+function StickMan() {
+    this.x = 0;
+    this.y = 0;
+    this.diameter = 35;
+    this.speed = 1;
+    
+    this.move = function() {
+        this.x += this.speed;
+        this.y -= this.speed;
+    };
+    
+    this.display = function() {
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }; 
 }
 
 function calculate() {
