3c83faf84b6308e230950d9c82deedfeff29ec24
Joseph Monaco <jcm132@pitt.edu>
2018-03-10 20:43:33 -0500

    It jumps the pole now

--- poles.js
+++ poles.js
@@ -1,60 +1,54 @@
 let jumpHeight = 0;
-let man;
 let xPos = 0;
 let yPos = 0;
 
+let x = 0;
+let y = 0;
+let time = 0;
+let diameter = 35;
+let speed = 0.8;
+
+const accel = 0.00001;
+
 function setup() {
     createCanvas(1024, 480);
-    man = new StickMan();
 }
 
 function draw() {
     background(50, 89, 100);
     let xOrg = 320;
     let yOrg = 240;
-    
+
     translate(xOrg, yOrg);
     strokeWeight(2);
-    // Stick figure
-    //line(xPos, yPos, xPos, yPos+75); // Body
-    //line(xPos, yPos+30, xPos+35, yPos+35); // Right Arm
-    //line(xPos, yPos+30, xPos-35, yPos+35); // Left Arm
-    //line(xPos, yPos+75, xPos+25, yPos+35+75); // Right Leg
-    //line(xPos, yPos+75, xPos-25, yPos+35+75); // Left Leg
-    //ellipse(xPos, yPos, 30, 30); // Head
-    for(let i=0; i < 100; i++) {
-        man.move();
-        man.display();
-    }
-    
-    xPos = xPos + 1;
-    yPos = yPos - 1;
-    if(xPos > 75) xPos = 0;
-    if(yPos < -200) yPos = 0;
-    
-    
+
+    setInterval(move(), 1000);
+    setInterval(display(), 1000);
+
     // Pole
-    translate(xOrg, yOrg-240);
+    //translate(xOrg, yOrg-240);
     strokeWeight(3);
-    line(0, -100, 0, 100);
-    line(50, -100, 50, 100);
-    line(0, -90, 50, -90);
+    line(125, -100, 125, 100);
+    line(150, -100, 150, 100);
+    line(125, -90, 150, -90);
 }
 
-function StickMan() {
-    this.x = 0;
-    this.y = 0;
-    this.diameter = 35;
-    this.speed = 1;
-    
-    this.move = function() {
-        this.x += this.speed;
-        this.y -= this.speed;
-    };
-    
-    this.display = function() {
-        ellipse(this.x, this.y, this.diameter, this.diameter);
-    }; 
+
+function move() {
+  if(x <= 140) {
+    x += speed;
+    y -= speed;
+  } else {
+    x += speed;
+    y += speed;
+  }
+  speed += accel;
+  //if(x >= 48+240 || x <= 48-240) speed = -speed;
+  time = time + 1;
+}
+
+function display() {
+  ellipse(x, y, diameter, diameter);
 }
 
 function calculate() {
